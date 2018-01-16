5331cf6a1ba755c99a954cb5163ba4172c8d6e6b
Christian Gellweiler <cgellweiler@gmail.com>
2018-01-16 13:38:36 +0100

    Add postgres db to docker setup

---
+++ docker/entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+
+cmd="$@"
+
+export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@db:5432/$POSTGRES_USER
+
+function postgres_ready(){
+pipenv run python << END
+import sys
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname="$POSTGRES_USER", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="db")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Postgres is up - continuing..."
+exec $cmd
